c66b48e99b350a1358d458b61adc96fe97db9d28
Norti333 <michael.glicks1991@gmail.com>
2017-06-08 14:39:36 +0300

    Prototype

---
+++ script.js
@@ -0,0 +1,46 @@
+    var bookInfo = {
+        books: [
+
+        ]
+    };
+
+    var fetch = function (isbn) {
+        var url = 'https://www.googleapis.com/books/v1/volumes?q='+$('#input').val() +':' + $('.input').val() + '&Pagination=maxResults'
+        $.ajax({
+            method: "GET",
+            url: url,
+            success: function (data) {
+                console.log(data);
+                
+                for (var i = 0; i < data.items.length; i++) {
+                    bookInfo.books.push({
+                        title: data.items[i].volumeInfo.title,
+                        author: data.items[i].volumeInfo.authors,
+                        description: data.items[i].volumeInfo.description,
+                        imgsrc: data.items[i].volumeInfo.imageLinks ? data.items[i].volumeInfo.imageLinks.thumbnail : '',
+                        link: data.items[i].volumeInfo.infoLink
+
+                    });
+                };
+                renderBook()
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log(textStatus);
+            }
+
+        })
+    }
+
+
+
+    var renderBook = function () {
+        $('.result').empty()
+        var source = $('#render-post').html();
+        var template = Handlebars.compile(source);
+        var newHTML = template(bookInfo);
+        $('.result').append(newHTML);
+
+    };
+
+
+    $('.search').click(fetch)
