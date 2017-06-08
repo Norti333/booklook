58d300534b04eab1ba97ebedad5567f4cf5be6c5
Norti333 <michael.glicks1991@gmail.com>
2017-06-08 15:26:52 +0300

    added a picture when one is not found

--- script.js
+++ script.js
@@ -5,19 +5,19 @@
     };
 
     var fetch = function (isbn) {
-        var url = 'https://www.googleapis.com/books/v1/volumes?q='+$('#input').val() +':' + $('.input').val() + '&Pagination=maxResults'
+        var url = 'https://www.googleapis.com/books/v1/volumes?q=' + $('.searchBy').val() + ':' + $('.input').val() + '&Pagination=maxResults'
         $.ajax({
             method: "GET",
             url: url,
             success: function (data) {
                 console.log(data);
-                
+
                 for (var i = 0; i < data.items.length; i++) {
                     bookInfo.books.push({
                         title: data.items[i].volumeInfo.title,
                         author: data.items[i].volumeInfo.authors,
                         description: data.items[i].volumeInfo.description,
-                        imgsrc: data.items[i].volumeInfo.imageLinks ? data.items[i].volumeInfo.imageLinks.thumbnail : '',
+                        imgsrc: data.items[i].volumeInfo.imageLinks ? data.items[i].volumeInfo.imageLinks.thumbnail : 'http://www.library.psychol.cam.ac.uk/images/GenericBook',
                         link: data.items[i].volumeInfo.infoLink
 
                     });
